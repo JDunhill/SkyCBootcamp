a78380e6f26f225e1521330979e93403792434f7
jdunhill <jtjdunhill@googlemail.com>
2023-08-22 11:41:23 +0100

    Added typedef

--- structs.c
+++ structs.c
@@ -4,15 +4,23 @@
 struct myStruct {
     int x;
     char y;
-};
+} var1;
+
+typedef struct myStruct str;
+
+typedef struct str2 {
+    int j;
+} str2;
 
 
 
 int main() {
 
-struct myStruct var1 = { 2, 'j'};
+str var1 = { 2, 'j'};
+str2 var2 = { 3 };
 printf("%d \n%c \n", var1.x, var1.y);
+printf("%d\n", var2.j);
 return 0;
 
 
-}
+}   
