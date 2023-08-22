90bed5005b97f98ad4de001f6a823403a5db5146
jdunhill <jtjdunhill@googlemail.com>
2023-08-22 16:22:30 +0100

    added counter static variable

--- functions.c
+++ functions.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-void talk(char* arr) {
+
+int talk(char* arr) {
     puts(arr);
+    static int counter = 0;
+    counter++;
+    return counter;
 }
 
 
 int main() {
 
     char arr[128] = {'h', 'e', 'l', 'l', 'o', '\0'};
-    talk(arr);
+
+    for(int i = 0; i < 10; i++) {
+        talk(arr);
+    }
+
+    printf("%d\n", talk(arr));
 
 }
